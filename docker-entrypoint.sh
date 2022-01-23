b64c918ff01c18672799c38124ab58a66b8ab1e7
Yaron Shahrabani <sh.yaron@gmail.com>
2022-01-23 18:00:33 +0200

    More fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/ash
-/root/go/bin/pritunl-zero mongo ${MONGO_URI:-mongodb://mongo:27017/pritunl-zero} set BastionDockerImage ${BASTION_IMAGE:-ghcr.io/yarons/pritunl-bastion}
+/root/go/bin/pritunl-zero mongo ${MONGO_URI:-mongodb://mongo:27017/pritunl-zero}
+/root/go/bin/pritunl-zero set system bastion_docker_image \"${BASTION_IMAGE:-ghcr.io/yarons/pritunl-bastion}\"
 /root/go/bin/pritunl-zero start
